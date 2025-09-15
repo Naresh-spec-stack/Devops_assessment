b7cf618d9d118abebf1a7eda3c8a979ea13e79b8
Naresh-spec-stack <naresh062497@gmail.com>
2025-09-15 20:41:26 +0530

    intialize database script

--- db/init.sql
+++ db/init.sql
@@ -1,1 +1,14 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    price NUMERIC(10,2)
+);
+
+INSERT INTO users (name) VALUES ('Ram'), ('Naresh'), ('Jia');
+INSERT INTO products (name, price) VALUES ('Laptop', 1200.50), ('Phone', 799.99);
